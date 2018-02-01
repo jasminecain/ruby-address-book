bf4aef4888827cbb35fb3884b0dad14e05160554
Jasmine Quach <xjasminequach@gmail.com>
2018-02-01 16:41:49 -0600

    Search by phone added

--- address_book.rb
+++ address_book.rb
@@ -7,6 +7,40 @@ class AddressBook
     @contacts = []
   end
 
+  def print_results(search, results)
+    puts search
+    results.each do |contact|
+      puts contact.to_s('full_name')
+      contact.print_phone_numbers
+      contact.print_addresses
+      puts "\n"
+    end
+  end
+
+  def find_by_name(name)
+    results = []
+    search = name.downcase
+    contacts.each do |contact|
+      if contact.full_name.downcase.include?(search)
+        results.push(contact)
+      end
+    end
+     print_results("Name search results (#{search})", results)
+  end
+
+  def find_by_phone_number(number)
+    results = []
+    search = number.gsub("-", "")
+    contacts.each do |contact|
+      contact.phone_numbers.each do |phone_number|
+        if phone_number.number.gsub("-", "").include?(search)
+          results.push(contact)
+        end
+      end
+    end
+    print_results("Phone search results (#{search})", results)
+  end
+
   def print_contact_list
     puts "Contact List"
     contacts.each do |contact|
@@ -33,4 +67,8 @@ juliet.add_address("Home", "111 Second St.", "", "San Francisco", "CA", "67890"
 address_book.contacts.push(jasmine)
 address_book.contacts.push(juliet)
 
-address_book.print_contact_list
+# address_book.print_contact_list
+
+# address_book.find_by_name("jasmine")
+address_book.find_by_name("a")
+address_book.find_by_phone_number("23")
