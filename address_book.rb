37d36e09d5ec064e17c66cb9ef5f879330742e3b
Jasmine Quach <xjasminequach@gmail.com>
2018-02-02 15:29:40 -0600

    Add command line menu

--- address_book.rb
+++ address_book.rb
@@ -7,6 +7,36 @@ class AddressBook
     @contacts = []
   end
 
+  def run
+    loop do
+      puts "Address Book"
+      puts "a: Add Contact"
+      puts "p: Print Address Book"
+      puts "e: Exit"
+      print "Enter your choice: "
+      input = gets.chomp.downcase
+      case input
+        when 'a'
+          add_contact
+        when 'p'
+          print_contact_list
+        when 'e'
+          break
+      end
+    end
+  end
+
+  def add_contact
+    contact = Contact.new
+    print "First name: "
+    contact.first_name = gets.chomp
+    print "Middle name:"
+    contact.middle_name = gets.chomp
+    print "Last name: "
+    contact.last_name = gets.chomp
+    contacts.push(contact)
+  end
+
   def print_results(search, results)
     puts search
     results.each do |contact|
@@ -63,26 +93,27 @@ class AddressBook
 end
 
 address_book = AddressBook.new
+address_book.run
 
-jasmine = Contact.new
-jasmine.first_name = "Jasmine"
-jasmine.last_name = "Quach"
-jasmine.add_phone_number("Home", "123-456-7890")
-jasmine.add_phone_number("Work", "234-567-8901")
-jasmine.add_address("Home", "123 Main St.", "", "Portland", "OR", "12345")
+# jasmine = Contact.new
+# jasmine.first_name = "Jasmine"
+# jasmine.last_name = "Quach"
+# jasmine.add_phone_number("Home", "123-456-7890")
+# jasmine.add_phone_number("Work", "234-567-8901")
+# jasmine.add_address("Home", "123 Main St.", "", "Portland", "OR", "12345")
 
-juliet = Contact.new
-juliet.first_name = "Juliet"
-juliet.last_name = "Lawson"
-juliet.add_phone_number("Home", "111-111-1111")
-juliet.add_address("Home", "111 Second St.", "", "San Francisco", "CA", "67890"
-)
-address_book.contacts.push(jasmine)
-address_book.contacts.push(juliet)
+# juliet = Contact.new
+# juliet.first_name = "Juliet"
+# juliet.last_name = "Lawson"
+# juliet.add_phone_number("Home", "111-111-1111")
+# juliet.add_address("Home", "111 Second St.", "", "San Francisco", "CA", "67890"
+# )
+# address_book.contacts.push(jasmine)
+# address_book.contacts.push(juliet)
 
-# address_book.print_contact_list
+# # address_book.print_contact_list
 
-# address_book.find_by_name("jasmine")
-# address_book.find_by_name("a")
-# address_book.find_by_phone_number("23")
-address_book.find_by_address("second")
+# # address_book.find_by_name("jasmine")
+# # address_book.find_by_name("a")
+# # address_book.find_by_phone_number("23")
+# address_book.find_by_address("second")
